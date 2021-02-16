5f6e911985a1d4f19112fdd36ab997060c5d3561
Pratyush Praveen <pratyush1296@gmail.com>
2021-02-16 13:37:44 +0530

    Updates

---
+++ precache-manifest.4e23d767df649546fa5cdd3f793c2580.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f05bc199cdd492534b23879a164a66",
+    "url": "/ToDo-React/index.html"
+  },
+  {
+    "revision": "3cfe72f6f7927756de39",
+    "url": "/ToDo-React/static/js/2.c04813ce.chunk.js"
+  },
+  {
+    "revision": "e7daa7f5d8a40e02a6b7",
+    "url": "/ToDo-React/static/js/main.a3a954a3.chunk.js"
+  },
+  {
+    "revision": "c591faaa5cd2e76fdd44",
+    "url": "/ToDo-React/static/js/runtime-main.1d8d7383.js"
+  }
+]);
